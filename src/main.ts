11ac5b4142e8faad07fb3e6f7d9a78de33a66cc8
Sang-NguyenMinh <nmsang.dev@gmail.com>
2025-12-13 12:34:51 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -26,10 +26,9 @@ async function bootstrap() {
   );
   app.enableCors({
     origin: [
-      'https://your-nextjs-app.vercel.app',
       'http://localhost:3000',
       'http://localhost:3001',
-      'https://ecommerce-frontend-xi-murex.vercel.app/',
+      'https://ecommerce-frontend-xi-murex.vercel.app',
     ],
     credentials: true,
   });
